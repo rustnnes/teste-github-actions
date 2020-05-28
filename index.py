dc79e39d153f6b9b13f51944985a147b3ac117e7
Diego Rustnnes Bandeira <diego.rustnnes@conductor.com.br>
2020-05-28 12:15:39 -0300

    test #1: get python path

---
+++ index.py
@@ -0,0 +1,16 @@
+# import unittest
+
+# def fun(x):
+#     return x + 1
+
+# class MyTest(unittest.TestCase):
+#     def test(self):
+#         self.assertEqual(fun(3), 5)
+
+print('test 0')
+assert sum([1, 2, 3]) == 6
+
+print('test 1')
+assert sum([1, 2, 3]) != 7
+
+print('all tests ok')
